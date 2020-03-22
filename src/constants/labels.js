5fc0b93c2298492e29c9ff5b0b5e38c9cec3f585
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-22 00:56:44 +0000

    modified constants

--- src/constants/labels.js
+++ src/constants/labels.js
@@ -1,7 +1,7 @@
 export const LABELS = {
 	NO_OF_DAYS: 'No. of Days between the date range',
 	NO_OF_MONDAYS: 'Number of Mondays between the date range',
-	START_DATE_LEAP_YEAR: 'Is End Date a Leap year?',
-	END_DATE_LEAP_YEAR: 'Is Starts Date a Leap year?',
-	FACTS_OF_START_DATE: 'Start Date Facts'
+	START_DATE_LEAP_YEAR: 'Is end date a Leap year?',
+	END_DATE_LEAP_YEAR: 'Is start date a Leap year?',
+	FACTS_OF_START_DATE: 'What happened on start day?'
 };
