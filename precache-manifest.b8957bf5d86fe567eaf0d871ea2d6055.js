5ed2a4d68bf5c446fc12c6c761839867888562e6
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-22 00:23:31 +0000

    Updates

---
+++ precache-manifest.b8957bf5d86fe567eaf0d871ea2d6055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7b0c34b66c4b244f3a7f38800a1034",
+    "url": "/date-picker-simple-app/index.html"
+  },
+  {
+    "revision": "7a04b654c74d5dff2390",
+    "url": "/date-picker-simple-app/static/css/2.c011c208.chunk.css"
+  },
+  {
+    "revision": "7a04b654c74d5dff2390",
+    "url": "/date-picker-simple-app/static/js/2.1eaf2c39.chunk.js"
+  },
+  {
+    "revision": "a7f3255fa0df0f6cfa1a8a25dc4c5c1d",
+    "url": "/date-picker-simple-app/static/js/2.1eaf2c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405e5c9aa2eab8ec071e",
+    "url": "/date-picker-simple-app/static/js/main.d7d070eb.chunk.js"
+  },
+  {
+    "revision": "99b760f634bab9239616",
+    "url": "/date-picker-simple-app/static/js/runtime-main.91f73a3a.js"
+  }
+]);
